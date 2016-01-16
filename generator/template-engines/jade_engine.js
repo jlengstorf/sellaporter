2f6c463c7c124d492f6d5a234aeb3f0844ea427e
jlengstorf <jason.lengstorf@copterlabs.com>
2016-01-16 14:32:00 +0700

    feat(jade): modified the Punch Jade template engine to pretty-print

---
+++ generator/template-engines/jade_engine.js
@@ -0,0 +1,39 @@
+/*
+* Template engine for Jade
+* Based on Jade - https://github.com/visionmedia/jade#readme
+*/
+
+var BaseEngine = require("punch").TemplateEngines.Base;
+var Jade = require("jade");
+
+var _ = require("underscore");
+var util = require('util');
+var path = require('path');
+
+function JadeEngine(options){
+	BaseEngine.call(this, options);
+
+	this.extension = JadeEngine.extension;
+	this.renderFunction = JadeEngine.renderFunction;
+};
+
+util.inherits(JadeEngine, BaseEngine);
+
+JadeEngine.extension = ".jade";
+JadeEngine.template_dir = null;
+
+JadeEngine.setup = function(config){
+	JadeEngine.template_dir = config.template_dir;
+}
+
+JadeEngine.renderFunction = function(template, content, partials, helpers) {
+	var content_with_helpers = _.extend({}, content, helpers.tag, helpers.block);
+
+	var compiled_template = Jade.compile(template, {
+    "basedir": path.join(process.cwd(), JadeEngine.template_dir),
+    "pretty": true,
+  });
+	return compiled_template(content_with_helpers);
+}
+
+module.exports = JadeEngine;
