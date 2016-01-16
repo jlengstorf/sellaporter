0b9528528600a00afd32c84739624a0f40d2528c
jlengstorf <jason.lengstorf@copterlabs.com>
2016-01-16 14:33:04 +0700

    feat(scripts): added scripts, set up for webpack
    Right now the JS only loads CSS, which seems silly, but will keep
    things simple as the app gets more complex.

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+/*
+ * # Sellaporter Scripts
+ * The entry point for all Sellaporter scripts.
+ */
+
+// Load styles into webpack. They're output to their own bundle.
+require('../styles/sellaporter.css');
