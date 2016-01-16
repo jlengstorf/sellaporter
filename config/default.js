ba8b7572d765d463325552e0cff79d29684a631e
jlengstorf <jason.lengstorf@copterlabs.com>
2016-01-16 15:55:20 +0700

    feat(config): combined config files for simplicity and flexibility
    Punch now loads config vars from the same config, which allows us
    to use env variables to keep things more flexible.

--- config/default.js
+++ config/default.js
@@ -1,7 +1,21 @@
 module.exports = {
+
+  // Punch config.
+  template_dir: 'templates',
+  output_dir: 'build',
+  server: {
+    port: process.env.PORT || 7500,
+  },
+  plugins: {
+    content_handler: './generator/content-handlers/wp_rest_api_content_handler',
+    template_engine: './generator/template-engines/jade_engine',
+  },
+
+  // WP REST API config — this is your blog endpoint.
   content: {
     host: process.env.PUNCH_API_HOST || '127.0.0.1',
     path: process.env.PUNCH_API_PATH || '/wordpress/wp-json/wp/v2',
     port: process.env.PUNCH_API_PORT || 7501,
   },
+
 };
