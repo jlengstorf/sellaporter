a214c0a207bc56c0bea61b86270ecf6481601aa9
jlengstorf <jason.lengstorf@copterlabs.com>
2016-01-16 14:31:39 +0700

    feat(content): created a content handler for Punch that reads the WP API

---
+++ generator/content-handlers/wp_rest_api_content_handler.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const util = require('util');
+const http = require('http');
+const config = require('getconfig');
+
+module.exports = {
+  isSection: basepath => basepath === '/',
+
+  negotiateContent: (basepath, file_extension, options, callback) => {
+    if (basepath === '/index' || basepath === '/200') {
+      return callback(null, {}, null, {});
+    }
+
+    const slug = basepath.substr(1);
+
+    // Check the query string for a `phase` parameter and modify the request.
+    let spOptions = '';
+    if (options.query.hasOwnProperty('phase')) {
+      spOptions = '&sellaporter[phase]=' + options.query['phase'];
+    }
+
+    const reqOptions = {
+      host: config.content.host,
+      port: config.content.port,
+      path: util.format('%s/pages?filter[name]=%s%s', config.content.path, slug, spOptions),
+      method: 'GET',
+    };
+
+    const req = http.request(reqOptions, res => {
+      let full_body = '';
+
+      res.setEncoding('utf-8');
+
+      res.on('data', chunk => {
+        full_body += chunk;
+      });
+
+      res.on('end', () => {
+        const pageData = {
+          slug: slug,
+          fields: JSON.parse(full_body).shift(),
+        };
+        if (pageData.fields !== 'undefined') {
+          return callback(null, pageData, {}, new Date().getTime());
+        }
+      });
+    });
+
+    req.on('error', error => {
+      return callback(util.format('[Error: %s]', error.message), null, null, {});
+    });
+
+    req.end();
+  },
+};
